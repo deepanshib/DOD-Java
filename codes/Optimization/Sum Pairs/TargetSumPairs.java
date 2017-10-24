c6ebb2cd0fe7368f431c86e04d90f1696ab92416
Deepanshi Bansal <deepanshibansal@ymail.com>
2017-10-25 01:26:25 +0530

    pair sum equal to target in n time complexity

--- codes/Optimization/Sum Pairs/TargetSumPairs.java
+++ codes/Optimization/Sum Pairs/TargetSumPairs.java
@@ -1,23 +1,44 @@
 /* You are given an array of size n and a target sum.
   You've to print all the possible pairs of numbers in the array that sum to the given target sum.
-  The time complexity of the code is O(n^2). Optimize the code to lower the time complexity.  
+  The time complexity of the code is O(n). 
  */
-
-public class Main{
-	
-    public static void main(String args[]){
-        int[] arr = { 1, 5, 7, -1, 5 };
-        int sum = 6;
-        TargetSumPairs(arr, sum);
-    }
-    
-    public static void TargetSumPairs(int[] arr, int sum){
-        int count = 0;
-        for (int i = 0; i < arr.length; i++)
-            for (int j = i + 1; j < arr.length; j++)
-                if ((arr[i] + arr[j]) == sum)
-                    count++;
+import java.util.HashMap;
  
-        System.out.println("Count of pairs is "+count);
+public class Main
+{
+    static int arr[] = new int[]{1, 5, 7, -1, 5} ;
+
+    static int getPairsCount(int n, int sum)
+    {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+ 
+       
+        for (int i=0; i<n; i++){
+             
+            if(!hm.containsKey(arr[i]))
+                hm.put(arr[i],0);
+                 
+            hm.put(arr[i], hm.get(arr[i])+1);
+        }
+        int twice_count = 0;
+ 
+      
+        for (int i=0; i<n; i++)
+        {
+            if(hm.get(sum-arr[i]) != null)
+                twice_count += hm.get(sum-arr[i]);
+
+            if (sum-arr[i] == arr[i])
+                twice_count--;
+        }
+        return twice_count/2;
+    }
+
+    public static void main(String[] args) {
+         
+        int sum = 6;
+        System.out.println("Count of pairs is " + 
+                            getPairsCount(arr.length,sum));
+         
     }
 }
